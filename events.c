13d3fc17cffbc43bd077c948cddb412c4ffb9cce
Petr Havlena <havlenapetr@gmail.com>
2010-08-18 23:59:41 +0200

    remove ANDROID_IBSS_HACK hack and disable android adhoc lock(just check if we aren't in mismatch)

--- events.c
+++ events.c
@@ -478,9 +478,10 @@ wpa_supplicant_select_bss(struct wpa_supplicant *wpa_s, struct wpa_ssid *group,
 				continue;
 			}
 
-			if (bss->caps & IEEE80211_CAP_IBSS) {
+			if ((bss->caps & IEEE80211_CAP_IBSS) &&
+					ssid->mode != IEEE80211_MODE_IBSS) {
 				wpa_printf(MSG_DEBUG, "   skip - "
-					   "IBSS (adhoc) network");
+					   "IBSS (adhoc) network mode mismatch");
 				continue;
 			}
 
